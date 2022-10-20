336637551ef11bea5593f744bc4716d52b636cf0
Linnik <MLinnik@it-one.ru>
2022-10-20 12:44:51 +0300

    Поправлены замечания по заданию 2 и 3.

--- sandbox/src/main/java/ru/stqa/pft/sandbox/PointDistance.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/PointDistance.java
@@ -2,7 +2,8 @@ package ru.stqa.pft.sandbox;
 
 public class PointDistance {
   public static void main(String[] args) {
-    Point p = new Point(2, 6, 3, 6);
-    System.out.println("Pасстояние между двумя точками " + "p1(" + p.x1 + ";" + p.y1 + ") и " + "p2(" + p.x2 + ";" + p.y2 + ")" + " = " + p.distance());
+    Point p1 = new Point(2, 3);
+    Point p2 = new Point(6, 6);
+    System.out.println("Pасстояние между двумя точками " + "p1(" + p1.x + ";" + p1.y + ") и " + "p2(" + p2.x + ";" + p2.y + ")" + " = " + p1.distance(p2));
   }
 }
--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointDistanceTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointDistanceTests.java
@@ -7,19 +7,22 @@ public class PointDistanceTests {
 
   @Test
   public void testDistance() {
-    Point p = new Point(2, 6, 3, 6);
-    Assert.assertEquals(p.distance(), 5.0);
+    Point p1 = new Point(2, 3);
+    Point p2 = new Point(6, 6);
+    Assert.assertEquals(p1.distance(p2), 5.0);
   }
 
   @Test
   public void testDistanceZero() {
-    Point p = new Point(0, 0, 0, 0);
-    Assert.assertEquals(p.distance(), 0.0);
+    Point p1 = new Point(0, 0);
+    Point p2 = new Point(0, 0);
+    Assert.assertEquals(p1.distance(p2), 0.0);
   }
 
   @Test
   public void testDistanceNegativeValues() {
-    Point p = new Point(1, 0, 1, 0);
-    Assert.assertEquals(p.distance(), 1.4142135623730951);
+    Point p1 = new Point(1, 0);
+    Point p2 = new Point(0, 1);
+    Assert.assertEquals(p1.distance(p2), 1.4142135623730951);
   }
 }
